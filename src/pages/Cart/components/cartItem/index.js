f2e343015aa1fbf7465f0175be4995844f783151
Jindrich Kytler <jindrich.kytler@gmail.com>
2019-04-08 00:12:34 +0200

    Fix negative numbers

--- src/pages/Cart/components/cartItem/index.js
+++ src/pages/Cart/components/cartItem/index.js
@@ -7,20 +7,25 @@ import {
 } from './styled'
 
 const cartItemComponent = ({ item, onRemoveFromCart }) => (
-  <CartItem key={item.product.id}>
-    <CartItemName>{item.product.name}</CartItemName>
-    <CartItemQuantity>{item.quantity}</CartItemQuantity>
-    <CartItemButton>
-      <button
-        onClick={evt => onRemoveFromCart(item.product.id, evt)}
-        type={'button'}
-        title={'Delete this cart item'}
-        aria-label={'Delete this cart item'}
-      >
-        x
-      </button>
-    </CartItemButton>
-  </CartItem>
+  <>
+    {item.quantity > 0 && (
+      <CartItem key={item.product.id}>
+        <CartItemName>{item.product.name}</CartItemName>
+        <CartItemQuantity>{item.quantity}</CartItemQuantity>
+        <CartItemButton>
+          <button
+            onClick={evt => onRemoveFromCart(item.product.id, evt)}
+            type={'button'}
+            title={'Delete this cart item'}
+            aria-label={'Delete this cart item'}
+            disabled={item.quantity < 1 && item.quantity}
+          >
+            x
+          </button>
+        </CartItemButton>
+      </CartItem>
+    )}
+  </>
 )
 
 export default cartItemComponent

9a9b195b73365733019c4454ce15e01be1a7ceb2
Jindrich Kytler <jindrich.kytler@gmail.com>
2019-04-20 16:58:49 +0200

    PageSize Label

--- src/pages/ProductList/index.js
+++ src/pages/ProductList/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import qs from 'qs'
+import { Link } from 'react-router-dom'
 
 import Layout from '../../components/Layout'
 import Loader from '../../components/Loader'
@@ -14,19 +15,21 @@ import * as productActions from '../../store/products/actions'
 import { ProductsWrap } from './styled'
 import Product from './components/Product'
 import Pagination from '../../components/Pagination'
+import urls from '../../constants/urls'
 
 const Products = ({ location, addProduct }) => {
   const { page } = qs.parse(location.search.substr(1))
-  const defautlPageSize = 10
+
+  const defaultPageSize = 10
 
   const handleOnChange = evt => {
-    if (evt.target.value) {
-      return evt.target.value
-    }
+    console.log(evt.target.value)
+    // 1. call some getProducts with size: evt.target.value
+    // but i cant call useEffect hook inside function
   }
 
   const { data: res, isLoading } = useApi(
-    () => getProducts({ page: { number: page, size: defautlPageSize } }),
+    () => getProducts({ page: { number: page, size: defaultPageSize } }),
     [page]
   )
 
@@ -39,22 +42,31 @@ const Products = ({ location, addProduct }) => {
       {res && (
         <>
           <div>
-            <label htmlFor="pageSize">Number of products on page: </label>
+            {`Product ${
+              page === '1' ? '1' : (page - 1) * defaultPageSize + 1
+            } to ${page === undefined ? defaultPageSize : ''} ${
+              page !== undefined &&
+              res.meta.record_count > defaultPageSize * page
+                ? defaultPageSize * page
+                : res.meta.record_count
+            } of ${res.meta.record_count} `}
             <select
               id="pageSize"
               onBlur={evt => handleOnChange(evt)}
               name="page_size"
             >
-              <option value={defautlPageSize}>{defautlPageSize}</option>
+              <option value={defaultPageSize}>{defaultPageSize}</option>
               <option value="50">50</option>
               <option value="75">75</option>
               <option value="100">100</option>
             </select>
+            {' per page'}
           </div>
           <Pagination
             pages={res.meta.page_count}
             recors={res.meta.record_count}
           />
+          <Link to={`${urls.productList}?page=1&page=20`}>sssss</Link>
           <ProductsWrap>
             {res.data.map(product => (
               <Product

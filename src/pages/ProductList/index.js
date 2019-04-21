562f2cf17702f6a655164a55400280210236affc
Jindrich Kytler <jindrich.kytler@gmail.com>
2019-04-22 00:05:52 +0200

    PageSize select

--- src/pages/ProductList/index.js
+++ src/pages/ProductList/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import qs from 'qs'
-import { Link } from 'react-router-dom'
 
 import Layout from '../../components/Layout'
 import Loader from '../../components/Loader'
@@ -10,26 +9,19 @@ import { H1 } from '../../components/Typography'
 import { getProducts } from '../../api/getProducts'
 import { useApi } from '../../api/useApi'
 import * as cartActions from '../../store/cartItems/actions'
-import * as productActions from '../../store/products/actions'
 
 import { ProductsWrap } from './styled'
 import Product from './components/Product'
 import Pagination from '../../components/Pagination'
 import urls from '../../constants/urls'
-/*import { changePageSize } from '../../store/products/actions'*/
 
-const Products = ({ location, addProduct }) => {
-  const { page } = qs.parse(location.search.substr(1))
+const Products = ({ location, addProduct, history }) => {
+  const { page, page_size } = qs.parse(location.search.substr(1))
 
-  const defaultPageSize = 10
-
-  /*const handleOnChange = evt => {
-    const pageSizes = evt.target.value
-    console.log (pageSizes)
-  }*/
+  const pageSize = page_size === undefined ? 25 : page_size
 
   const { data: res, isLoading } = useApi(
-    () => getProducts({ page: { number: page, size: defaultPageSize } }),
+    () => getProducts({ page: { number: page, size: pageSize } }),
     [page]
   )
 
@@ -38,7 +30,6 @@ const Products = ({ location, addProduct }) => {
   return (
     <Layout>
       <H1 textAlign="center">E-Commerce app</H1>
-      <div>sss</div>
       {isLoading && <Loader />}
       {res && (
         <>
@@ -49,16 +40,16 @@ const Products = ({ location, addProduct }) => {
             ${page !== undefined && page === '1' ? '1' : ''}
             ${
               page !== undefined && page !== false && page !== '1'
-                ? (page - 1) * defaultPageSize + 1
+                ? (page - 1) * pageSize + 1
                 : ''
             }
             to
-            ${page === undefined ? defaultPageSize : ''}
-            ${page !== undefined && page === '1' ? defaultPageSize : ''}
+            ${page === undefined ? pageSize : ''}
+            ${page !== undefined && page === '1' ? pageSize : ''}
             ${
               page !== undefined && page !== false && page !== '1'
-                ? res.meta.record_count > defaultPageSize * page
-                  ? defaultPageSize * page
+                ? res.meta.record_count > pageSize * page
+                  ? pageSize * page
                   : res.meta.record_count
                 : ''
             }
@@ -66,18 +57,20 @@ const Products = ({ location, addProduct }) => {
             <select
               id="pageSize"
               onBlur={evt => {
-                evt.preventDefault()
+                history.push(
+                  `${urls.productList}?page=1&page_size=${evt.target.value}`
+                )
+                console.log(location.search)
               }}
               name="page_size"
             >
-              <option value={defaultPageSize}>{defaultPageSize}</option>
+              <option value="25">25</option>
               <option value="50">50</option>
               <option value="75">75</option>
               <option value="100">100</option>
             </select>
             {' per page'}
           </div>
-          <Link to={`${urls.productList}`}>asdada</Link>
           <Pagination
             pages={res.meta.page_count}
             recors={res.meta.record_count}
@@ -99,7 +92,6 @@ const Products = ({ location, addProduct }) => {
 
 const mapDispatchToProps = {
   addProduct: cartActions.addProduct,
-  changeProductList: productActions.changePageSize,
 }
 
 const ProductList = connect(

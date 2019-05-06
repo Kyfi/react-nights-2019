ad97bbd6037776de69e4a3df0ca8cd77a0059e10
Jindrich Kytler <jindrich.kytler@gmail.com>
2019-05-06 12:02:26 +0200

    Add Dotenv to Next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
+// eslint-disable-next-line no-console
+require('dotenv').config()
+
 const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
-module.exports = withTypescript(withCSS())
+module.exports = withTypescript(
+  withCSS({
+    cssModules: true,
+    webpack: config => {
+      config.plugins = config.plugins || []
+
+      config.plugins = [
+        ...config.plugins,
+
+        // Read the .env file
+        new Dotenv({
+          path: path.join(__dirname, '.env.development.local'),
+          systemvars: true,
+        }),
+      ]
+
+      return config
+    },
+  })
+)
--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export default {
-  clientId: '5e20f35514190d1c38c4a9a34839c0a12b7dc6d4b2074ba977e980204f94a591',
-  clientSecret:
-    'ed08767e3b33d9693359ca2756b781fb3d1199985fafbda346bf0f4a5b29c672',
-  scope: 'market:514',
-  apiUrl: 'https://react-nights.commercelayer.io',
+  clientId: process.env.REACT_APP_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+  scope: process.env.REACT_APP_SCOPE,
+  apiUrl: process.env.REACT_APP_APIURL,
 }
